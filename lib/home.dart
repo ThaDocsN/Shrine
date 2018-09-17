6739906f6609669b88535096a3f11d01a7901b67
ThaDocIzN <rodan72@msn.com>
2018-09-16 19:05:20 -0500

    modified app theme color

--- lib/home.dart
+++ lib/home.dart
@@ -13,6 +13,9 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'model/products_repository.dart';
+import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
   // TODO: Make a collection of cards (102)
@@ -29,22 +32,28 @@ class HomePage extends StatelessWidget {
               Icons.menu,
               semanticLabel: "menu",
             ),
-            onPressed: (){
+            onPressed: () {
               print("Menu button");
             }),
         title: Text("SHRINE"),
         actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.search,
-              semanticLabel: "search",),
-              onPressed: (){
+              icon: Icon(
+                Icons.search,
+                semanticLabel: "search",
+              ),
+              onPressed: () {
                 print("Search button");
               }),
-          IconButton(icon: Icon(Icons.tune,
-          semanticLabel: "filter",),
-              onPressed: (){
-            print("Filter button");
-              },),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: "filter",
+            ),
+            onPressed: () {
+              print("Filter button");
+            },
+          ),
         ],
       ),
       // TODO: Add a grid view (102)
@@ -52,25 +61,56 @@ class HomePage extends StatelessWidget {
         crossAxisCount: 2,
         padding: EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        children: <Widget>[Card(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              AspectRatio(aspectRatio: 18.0 / 11.0,
-              child: Image.asset("assets/diamond.png"),),
-              Padding(padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text("Title"),
-                  SizedBox(height: 8.0),
-                  Text("Secondary Text"),
-                ],
-              ),)
-            ],
-          ),
-        )],
+       children: _buildGridCards(context),
       ),
     );
+  }
+  List<Card> _buildGridCards(BuildContext context){
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if(products == null || products.isEmpty){
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+      locale: Localizations.localeOf(context).toString()
+    );
+    return products.map((product){
+      return Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+                aspectRatio: 18 / 11,
+            child: Image.asset(
+              product.assetName,
+              package: product.assetPackage,
+              fit: BoxFit.fitWidth,
+            ),),
+            Expanded(
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        product.name,
+                        style: theme.textTheme.title,
+                        maxLines: 1,
+                      ),
+                      SizedBox(height: 8.0),
+                      Text(
+                        formatter.format(product.price),
+                        style: theme.textTheme.body2,
+                      ),
+                    ],
+                  ),
+                ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
   }
 }
